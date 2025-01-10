189ad034b3b080a63fde4d69e9a75f6d76b8fc5a
Derek Johnson <siestaderek@gmail.com>
2025-01-10 02:25:23 -0700

    created accounts and deals schemas

--- server/db.ts
+++ server/db.ts
@@ -13,6 +13,38 @@ function initializeDatabase() {
   `
   ).run();
   // TODO: Add your account and deal tables schemas here
+
+  // Create ACCOUNTS schema:
+  db.prepare(
+    `
+    CREATE TABLE IF NOT EXISTS accounts (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      organization_id INTEGER NOT NULL,
+      account_name TEXT NOT NULL,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (organization_id) REFERENCES organizations(id)
+    );
+    `
+  ).run();
+
+  //Create DEALS schema:
+  db.prepare(
+    `
+  CREATE TABLE IF NOT EXISTS deals (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    account_id INTEGER NOT NULL,
+    deal_name TEXT NOT NULL,
+    value REAL NOT NULL,
+    status TEXT NOT NULL,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (account_id) REFERENCES accounts(id)
+  );
+  `
+  ).run();
   return db;
 }
 
